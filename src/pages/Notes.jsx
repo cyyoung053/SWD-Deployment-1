06270afef2320aa626e5677b48b6d6c886d8ac42
cyyoung053 <39162803+cyyoung053@users.noreply.github.com>
2025-01-19 12:24:11 -0500

    Update Notes.jsx

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,8 +1,8 @@
+import React, { useState, useEffect } from "react";
 import { CiSearch } from "react-icons/ci";
 import { Link } from "react-router-dom";
 import { BsPlusLg } from "react-icons/bs";
 import { MdClose } from "react-icons/md";
-import { useState, useEffect } from "react";
 
 import NoteItem from "../components/NoteItem";
 
@@ -11,50 +11,46 @@ const Notes = ({ notes }) => {
     const [text, setText] = useState("");
     const [filteredNotes, setFilteredNotes] = useState(notes);
 
-    const handleSearch = () => {
-        setFilteredNotes(
-            notes.filter((note) => {
-                if (note.title.toLowerCase().match(text.toLowerCase())) {
-                    return notes;
-                }
-                return true;
-            })
-        );
-    };
-    // eslint-disable-next-line
-    useEffect(handleSearch, [text]);
+    useEffect(() => {
+        const handleSearch = () => {
+            setFilteredNotes(
+                notes.filter((note) =>
+                    note.title.toLowerCase().includes(text.toLowerCase())
+                )
+            );
+        };
+        handleSearch();
+    }, [text, notes]);
 
     return (
         <section>
-            <header className='notes__header'>
+            <header className="notes__header">
                 {!showSearch && <h2>Notes</h2>}
                 {showSearch && (
                     <input
-                        type='text'
+                        type="text"
                         value={text}
-                        onChange={(e) => {
-                            setText(e.target.value);
-                            handleSearch();
-                        }}
+                        onChange={(e) => setText(e.target.value)}
                         autoFocus
-                        placeholder='Keyword...'
+                        placeholder="Keyword..."
                     />
                 )}
                 <button
-                    className='btn'
-                    onClick={() => setShowSearch((prevState) => !prevState)}>
+                    className="btn"
+                    onClick={() => setShowSearch((prevState) => !prevState)}
+                >
                     {showSearch ? <MdClose /> : <CiSearch />}
                 </button>
             </header>
-            <div className='notes__container'>
+            <div className="notes__container">
                 {filteredNotes.length === 0 && (
-                    <p className='empty__notes'>Notes NOT Found</p>
+                    <p className="empty__notes">Notes NOT Found</p>
                 )}
                 {filteredNotes.map((note) => (
                     <NoteItem key={note.id} note={note} />
                 ))}
             </div>
-            <Link to='/create' className='btn add__btn'>
+            <Link to="/create" className="btn add__btn">
                 <BsPlusLg />
             </Link>
         </section>
